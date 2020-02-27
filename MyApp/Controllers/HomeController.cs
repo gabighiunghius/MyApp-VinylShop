24d9bfc7f041ae23ce81f2fbe8c5d1aa5c5a75ab
gabighiunghius <56878112+gabighiunghius@users.noreply.github.com>
2020-02-27 19:39:00 +0200

    good

---
+++ MyApp/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using MyApp.DAL;
+using MyApp.Models;
+using MyApp.Models.Home;
+using MyApp.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index(string search, int? page)
+        {
+            HomeIndexViewModel model = new HomeIndexViewModel();
+            return View(model.CreateModel(search, 4, page));
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+    }
+}
